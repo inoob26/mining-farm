980c5221a2867d79803f2eb1d50527cac8d1622e
inoob26 <inoob26@gmail.com>
2018-01-10 00:06:04 +0300

    change driver from 384 to 387

--- driver_install.sh
+++ driver_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update
-sudo apt-get install nvidia-384 nvidia-384-dev nvidia-settings nvidia-cuda-toolkit
+sudo apt-get install nvidia-387 nvidia-387-dev nvidia-settings nvidia-cuda-toolkit
 
 #install text editor
 sudo apt-get install nano -y
