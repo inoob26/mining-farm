71776e156b55bbbf08adcde1ac764a0c983382e0
inoob26 <inoob26@gmail.com>
2018-01-14 14:56:14 +0300

    fix cuda-9-0

--- cuda_install.sh
+++ cuda_install.sh
@@ -3,7 +3,7 @@ wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1704/x86_64/c
 sudo dpkg -i cuda-repo-ubuntu1704_9.0.176-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1704/x86_64/7fa2af80.pub
 sudo apt-get update
-sudo apt-get install cuda
+sudo apt-get install cuda-9-0
 
 cd
 echo export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH >> .bashrc
